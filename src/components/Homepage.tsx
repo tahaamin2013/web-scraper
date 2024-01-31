ed3ae3608e000e9c85d4023d369f898a7dc04ffd
tahaamin2013 <tahaamindob2013@gmail.com>
2024-01-31 16:17:06 +0500

    fsdsd

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -27,9 +27,6 @@ const Homepage = () => {
     Explore real-time download stats. Enter a package name, click &quot;Go,&quot; and get quick insights. Simplifying package popularity analysis!
   </p>
 </div>
-
-
-
       <div className="flex flex-col sm:flex-row gap-4 sm:gap-2">
         <Input placeholder="Enter package name" value={input} onChange={(e) => setInput(e.target.value)} className="w-80 h-[46px] shadow-xl" />
         <Button className="px-[30px] py-[23px] shadow-2xl" onClick={getDownloads}>Go</Button>
